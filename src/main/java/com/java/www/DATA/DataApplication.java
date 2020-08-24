a1baf0b39d221f88d3a17cc4e255b54b93443873
zainab <zainabharoon1993@gmail.com>
2020-08-24 12:46:32 -0400

    Initial commit

---
+++ src/main/java/com/java/www/DATA/DataApplication.java
@@ -0,0 +1,10 @@
+
+package com.java.www.DATA;
+		import org.springframework.boot.SpringApplication;
+		import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class DataApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DataApplication.class, args);
+	}
+}
